ed667c42ae95b982e4c2b2971e3657752e6398e1
nikuvsonline <nku2007@hotmail.com>
2015-06-19 22:24:22 +0300

    Basic Talk App

--- Challenge1/Challenge1/Program.cs
+++ Challenge1/Challenge1/Program.cs
@@ -6,11 +6,88 @@ using System.Threading.Tasks;
 
 namespace Challenge1
 {
+    enum AccentType
+    {
+        Normal,
+        Happy,
+        Angry,
+        Hm
+    }
+
     class Program
     {
+        private static void PrintText(string text, AccentType accType)
+        {
+           switch (accType)
+           {
+               case AccentType.Normal: Console.ForegroundColor = ConsoleColor.White;
+                   break;
+               case AccentType.Angry: Console.ForegroundColor = ConsoleColor.Red;
+                   break;
+               case AccentType.Happy: Console.ForegroundColor = ConsoleColor.Green;
+                   break;
+               case AccentType.Hm: Console.ForegroundColor = ConsoleColor.DarkYellow;
+                   break;
+           }
+           Console.WriteLine(text);
+        }
+
+        private static AccentType GetAccentType(string answer)
+        {
+            var result = AccentType.Normal;
+            if (answer.Contains(":)"))
+            {
+                result = AccentType.Happy;
+            }
+            if (answer.Contains(":("))
+            {
+                result = AccentType.Angry;
+            }
+            if (answer.Contains(":O"))
+            {
+                result = AccentType.Hm;
+            }
+            if (result == AccentType.Happy)
+            {
+                myCurrType = result;
+            }
+            else if (result != AccentType.Angry)
+            {
+                myCurrType = result;
+            }
+            return result;
+        }
+
+        static AccentType myCurrType = AccentType.Normal;
+
+        private static string GetAnswer(string question)
+        {
+            var text = question.ToLower();
+            if (text.Contains("tavo vardas"))
+            {
+                return "Mano vardas yra Airi";
+            }
+            if (text.Contains("labas"))
+            {                
+                return "Labas";
+            }
+            if (text.Contains("kas tu"))
+            {
+                return "Na aš tiesiog eilinė asmenybė, o kas tu?";
+            }
+            return "Nesupratau, gal galėtum paaiškinti";
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+            PrintText("Hello World!!!", AccentType.Angry);
+            while (true)
+            {
+                var text = Console.ReadLine();
+                var answer = GetAnswer(text);
+                PrintText(answer, GetAccentType(answer));
+            }
             Console.ReadLine();
         }
     }
