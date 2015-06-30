9061fac1798aa7b7175bc1e5d1c9608c05a6b467
artisb45 <artis.b45@gmail.com>
2015-06-30 13:20:05 +0300

    Pirma pamoka naudojant Unity 5.1
    Zaidimo pradzia. Zaidejo judejimas ir skirtingi variantai.

---
+++ pamokaUnity/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundCheck : MonoBehaviour {
+
+	private PlayerMove player;
+
+	void Start(){
+		player = gameObject.GetComponentInParent<PlayerMove>();
+	}
+
+	void OnTriggerEnter(Collider col){
+	
+		player.isGrounded = true;
+	
+	}
+
+	void OnTriggerExit(Collider col){
+	
+		player.isGrounded = false;
+	
+	}
+}
---
+++ pamokaUnity/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+	public Rigidbody player;
+	public float speed = 10f;
+	public float jumpForce = 10f;
+	public bool isGrounded;
+
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate (){
+		player.AddForce (player.transform.forward * speed * Input.GetAxis("Vertical")); //Vector3.forward
+		if(Input.GetAxis("Jump") > 0.1f && isGrounded){
+			player.AddForce(new Vector3(player.velocity.x, jumpForce, player.velocity.z));
+		}
+		if (player.velocity.z > speed)
+			player.velocity = new Vector3 (player.velocity.x, player.velocity.y, speed);
+	}
+}
