6458bfddf9a2fb76400c08df5145731cb4bc1217
niku-live <nku2007@gmail.com>
2015-07-08 11:17:55 +0300

    7 tema 2 užduotis
    7 tema 2 užduotis

---
+++ MiniProblems/7 tema - ASP.NET/Vol7Ex2/Vol7Ex2/Vol7Ex2/test.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Vol7Ex2
+{
+    public partial class test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            System.IO.FileInfo finfo = new System.IO.FileInfo("C:\\Devel\\test.txt");
+            if (finfo.Exists)
+            {
+                System.IO.StreamReader reader = finfo.OpenText();
+                string text = reader.ReadLine();
+                reader.Close();
+                TextBox1.Text = text;
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string text = TextBox1.Text;
+            Label3.Text = text;
+            TextBox1.Text = "";
+            System.IO.FileInfo finfo = new System.IO.FileInfo("C:\\Devel\\test.txt");
+            System.IO.StreamWriter writer = finfo.CreateText();
+            writer.WriteLine(text);
+            writer.Close();
+        }
+    }
+}
